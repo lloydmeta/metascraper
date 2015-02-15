a99585c42d7dd9ede00f2b8e4684ad3877b753ca
Lloyd <lloydmeta@gmail.com>
2015-02-15 19:32:38 +0900

    Update readme

--- src/main/scala/com/beachape/metascraper/Messages.scala
+++ src/main/scala/com/beachape/metascraper/Messages.scala
@@ -16,14 +16,14 @@ object Messages {
    * @param acceptLanguageCode language to request for the page, defaults to 'en'
    * @param userAgent user agent to request as, defaults to 'Metascraper'
    * @param numberOfImages number of images links to return, defaults to 6
-   * @param schemas Schemas to use for scraping, defaults to Seq(HtmlSchemas(OpenGraph, and NormalPage)),
-   *                The schemas to the left of the list take precednece over those on the right
+   * @param schemaFactories Schema factories to use for scraping, defaults to Seq(HtmlSchemas(OpenGraph, and NormalPage)),
+   *                        The factories to the left of the list take precednece over those on the right
    */
   sealed case class ScrapeUrl(url: Url,
     acceptLanguageCode: String = "en",
     userAgent: String = "Metascraper",
     numberOfImages: Int = 5,
-    schemas: Seq[SchemaFactory] = Seq(HtmlSchemas(OpenGraph, NormalPage)))
+    schemaFactories: Seq[SchemaFactory] = Seq(HtmlSchemas(OpenGraph, NormalPage)))
 
   sealed case class ScrapedData(url: Url,
     title: String,
