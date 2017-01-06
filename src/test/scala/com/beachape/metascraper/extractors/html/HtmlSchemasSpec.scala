4df27a2adcc0d91845a5186a83291e5f0de20cd7
Alexander Azarov <azarov@osinka.ru>
2017-01-06 11:34:00 +0200

    Fix: correct doctypes in test data

--- src/test/scala/com/beachape/metascraper/extractors/html/HtmlSchemasSpec.scala
+++ src/test/scala/com/beachape/metascraper/extractors/html/HtmlSchemasSpec.scala
@@ -75,16 +75,16 @@ X-Frame-Options: SAMEORIGIN
 
   val Html =
     """
-      |<!DOCTYPE html>
-      |<html lang="ru"><head>
+      |<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+      |<html><head>
       |<meta name="result" content="CHARSET" />
       |</head></html>
     """.stripMargin
 
   val Html_httpequiv =
     """
-      |<!DOCTYPE html>
-      |<html lang="ru"><head>
+      |<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+      |<html><head>
       |<meta http-equiv="Content-Type" content="text/html; charset=windows-1251" />
       |<meta name="result" content="CHARSET" />
       |</head></html>
